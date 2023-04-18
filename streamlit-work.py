93149550b9842f120808ecaeb60a8a6c47ac0e07
Carson Buttars <carsonbuttars13@gmail.com>
2023-04-18 16:29:38 -0600

    Initial commit

---
+++ streamlit-work.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Load the data
+df = pd.read_csv('~/homework/Stat_386/class-participation/popular_names.csv')
+
+# Set up the dashboard
+st.title('Popular Baby Names')
+
+# Get user input
+genders = ['M', 'F']
+selected_name = st.text_input('Name','John')
+selected_gender = st.selectbox('Gender', genders)
+
+# Making the graph
+Title = 'Popularity of ' + selected_name + ' by year since 1910'
+fig = px.line(df.loc[df.name == selected_name, :].loc[df.sex == selected_gender], x = 'year', y = 'n', hover_data = ['n'])
+fig.update_layout(title = Title)
+
+# Delpoy the graph
+st.plotly_chart(fig)
